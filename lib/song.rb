3f893ed2cf7fcdb19868fd7eaecddd92aab9117c
Alex Engelhart <asengelhart@yahoo.com>
2019-12-19 17:42:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,5 +11,20 @@ class Song
   end
   
   def self.create
+    new_song = self.new 
+    new_song.tap{|song| song.save}
   end 
+  
+  def self.new_by_name(name)
+    new_song = self.new 
+    new_song.tap {|song| song.name = name}
+  end 
+  
+  def self.create_by_name(name)
+    new_song = self.create 
+    new_song.tap {|song| song.name = name}
+  end 
+  
+  def self.find_by_name(name)
+    
 end
